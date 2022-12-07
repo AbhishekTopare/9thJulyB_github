be505a154eba69a21742a1604cd988c67c283223
NEXUS <NEXUS@DESKTOP-2AO5O21>
2022-12-07 15:16:52 +0530

    [Abhishek]

---
+++ src/Sample/demo3.java
@@ -0,0 +1,15 @@
+package Sample;
+
+public class demo3 
+{
+  public static void main(String[] args) 
+  {
+	int a=20;
+	int b=30;
+	
+	System.out.println(a+b);
+	System.out.println(a*b);
+	System.out.println(a/b);
+	System.out.println(a%b);
+  }
+}
